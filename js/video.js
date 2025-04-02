3788e4193e81428844d7557bcc543f355257c47c
Matthew <113375550+msilberf1@users.noreply.github.com>
2025-04-02 17:47:14 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,71 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	console.log('auto play is set to ' + video.autoplay);
+	video.loop = false;
+	console.log('loop is set to ' + video.loop);
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Video");
+	video.playbackRate -= 0.1;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up Video");
+	video.playbackRate += 0.1;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip Video");
+	video.currentTime += 10;
+	console.log(video.currentTime);
+	if (video.currentTime >= 67.40333) {
+		video.currentTime = 0;
+		console.log("video ended");
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute Video");
+	if (video.muted === false) {
+		this.innerHTML = "Unmute";
+		video.muted = true;
+	}
+	else {
+		this.innerHTML = "Mute";
+		video.muted = false;
+	}
+});
+
+document.querySelector("#slider").addEventListener("input", function() {
+	console.log("Slide");
+	video.volume = this.value/100;
+	console.log(video.volume);
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("old school");
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("new school");
+	video.classList.remove("oldSchool");
+});
+
